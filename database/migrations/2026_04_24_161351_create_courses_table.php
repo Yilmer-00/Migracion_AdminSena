20ab964ef40e09b3ff786ff673cbad8d9dce03c5
enmanuel steven <stevenhernandezfz11@gmail.com>
2026-04-24 12:00:49 -0500

    termino en casa

---
+++ database/migrations/2026_04_24_161351_create_courses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('course number');
+            $table->string('day');
+            $table->unsignedBigInteger('area_id');
+
+            $table->foreign('area_id')
+            ->references('id')
+            ->on('areas')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+            $table->unsignedBigInteger('trainig_centers');
+
+            $table->foreign('computer_id')
+            ->references('id')
+            ->on('trainig_centers')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
