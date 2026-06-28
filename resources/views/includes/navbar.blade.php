a080de94abb731eacfc889b6abf677d3d0d7a884
Yilmer-00 <yilmermelengec@gmail.com>
2026-06-27 21:08:02 -0500

    Entrega pt2 de avance de navegacion con inclodes y layouts

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="{{route('area.create')}}">Area</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -18,9 +18,9 @@
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="{{route('computer.create')}}">nuevo computador</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="{route('teacher.create')}">Instructores</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="{route{('course.registro')}}">curso</a></li>
           </ul>
         </li>
         <li class="nav-item">
